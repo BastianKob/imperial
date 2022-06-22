e5fe98439a41d14bbd404b8594a9e3d455be5829
BastianKob <105808602+BastianKob@users.noreply.github.com>
2022-06-22 15:01:11 +0200

    added little frontend and calculation functionality

--- src/main/java/com/example/demo/ImperialAPI.java
+++ src/main/java/com/example/demo/ImperialAPI.java
@@ -1,14 +1,34 @@
 package com.example.demo;
 
 import javax.persistence.Entity;
-
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
 
 @Entity
 public class ImperialAPI {
+    @Id
+    public int id;
+    private double inches;
+    private double centimeters;
+    private double yards;
+    private double meters;
+
+    ImperialAPI(int id, double inches, double yards){
+        this.id = id;
+        this.inches = inches;
+        this.centimeters = InchToCentimeter(inches);
+        this.yards = yards;
+        this.meters = YardToMeter(yards);
+    }
     public double InchToCentimeter(double inch){
         return inch * 2.54;
     }
     public double YardToMeter(double yard){
         return yard * 0.9144;
+    }
+
+    @Override
+    public String toString() {
+        return "ImperialAPI{inches=" + inches + ", centimeters=" + centimeters + ", yards=" + yards + ", meters=" + meters + "}";
     }
 }
--- src/main/java/com/example/demo/ImperialApplication.java
+++ src/main/java/com/example/demo/ImperialApplication.java
@@ -2,15 +2,38 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 
 @SpringBootApplication
+@RestController
 public class ImperialApplication {
 
+	public ImperialAPI imp = new ImperialAPI(0, 0,0);
 	public static void main(String[] args) {
 		SpringApplication.run(ImperialApplication.class, args);
 	}
 
+	@GetMapping("/")
+	public String get(@RequestParam(name = "inches") double inches,@RequestParam(name = "inches") double yards){
+		imp = new ImperialAPI(0, inches, yards);
+		return imp.toString() + "<form>" +
+				"  <label for=\"inches\">Inches:</label><br>\n" +
+				"  <input type=\"number\" id=\"inches\" name=\"inches\" value=\"John\"><br>\n" +
+				"  <label for=\"yards\">Yards:</label><br>\n" +
+				"  <input type=\"number\" id=\"yards\" name=\"yards\" value=\"Doe\"><br><br>\n" +
+				"  <input type=\"submit\" value=\"Submit\">" +
+				"</form>";
+	}
+
+	@PostMapping("/")
+	public int post(){
+		return 0;
+	}
 }
